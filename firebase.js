33805133c89c8ed1f56a524614e650920e48ce44
Patrick Moreira <131713614+PatrickMoreiraMorais@users.noreply.github.com>
2024-03-28 18:31:32 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,1 @@
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC29TcLEqO0g59A_L_OzTNXJITCYhpaG2o",
-  authDomain: "at-dr3-api.firebaseapp.com",
-  databaseURL: "https://at-dr3-api-default-rtdb.firebaseio.com",
-  projectId: "at-dr3-api",
-  storageBucket: "at-dr3-api.appspot.com",
-  messagingSenderId: "581782072165",
-  appId: "1:581782072165:web:c97e1e88001ae231aa0f8c",
-};
-
-const app = initializeApp(firebaseConfig);
-export default app;
+//
